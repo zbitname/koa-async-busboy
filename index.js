b4d3db9cd3643f8d587551c7d37004f0f7bdf103
Alex K <Alex K>
2017-01-26 23:47:49 +0300

    Create methods for define functors for limit error events

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Busboy = require('busboy');
 
 module.exports = class AsyncBusboy {
+  /**
+   * Creates an instance of AsyncBusboy.
+   *
+   * @param {any} opts
+   * This object is argument for busboy constructor (https://github.com/mscdex/busboy#busboy-methods)
+   */
   constructor(opts) {
     if (!opts.headers) {
       throw new Error('Miss headers');
@@ -9,18 +15,70 @@ module.exports = class AsyncBusboy {
     this.busboy = new Busboy(opts);
     this._onFile = null;
     this._onField = null;
+    this._onFilesLimit = null;
   }
 
+  /**
+   * Function executed on event "file"
+   *
+   * @param {function} func
+   * @return {AsyncBusboy} this
+   */
   onFile(func) {
     this._onFile = func;
     return this;
   }
 
+  /**
+   * Function executed on event "field"
+   *
+   * @param {function} func
+   * @return {AsyncBusboy} this
+   */
   onField(func) {
     this._onField = func;
     return this;
   }
 
+  /**
+   * Function executed on event "filesLimit"
+   *
+   * @param {function} func
+   * @return {AsyncBusboy} this
+   */
+  onFilesLimit(func) {
+    this._onFilesLimit = func;
+    return this;
+  }
+
+  /**
+   * Function executed on event "partsLimit"
+   *
+   * @param {function} func
+   * @return {AsyncBusboy} this
+   */
+  onPartsLimit(func) {
+    this._onPartsLimit = func;
+    return this;
+  }
+
+  /**
+   * Function executed on event "fieldsLimit"
+   *
+   * @param {function} func
+   * @return {AsyncBusboy} this
+   */
+  onFieldsLimit(func) {
+    this._onFieldsLimit = func;
+    return this;
+  }
+
+  /**
+   * Request object for pipe busboy
+   *
+   * @param {any} req
+   * @return {Promise}
+   */
   pipe(req) {
     if (!this.onFile) {
       throw new Error('Miss describe this.onFile');
@@ -31,6 +89,36 @@ module.exports = class AsyncBusboy {
 
       if (this._onField) {
         this.busboy.on('field', this._onField);
+      }
+
+      if (this._onFilesLimit) {
+        this.busboy.on('filesLimit', () => {
+          try {
+            this._onFilesLimit();
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this._onPartsLimit) {
+        this.busboy.on('partsLimit', () => {
+          try {
+            this._onPartsLimit();
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+      if (this._onFieldsLimit) {
+        this.busboy.on('fieldsLimit', () => {
+          try {
+            this._onFieldsLimit();
+          } catch (e) {
+            reject(e);
+          }
+        });
       }
 
       this.busboy.on('finish', function() {
